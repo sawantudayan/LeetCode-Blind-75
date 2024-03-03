35bbbec19eb0c92c8b39f703f94f5c8ce3c632c8
Udayan Sawant <udayansawant7@gmail.com>
2024-03-03 14:33:28 -0800

    DAY 2 - Arrays & Strings

---
+++ Arrays & Strings/345_Reverse_Vowels_Of_String.py
@@ -0,0 +1,27 @@
+def reverseVowels(self, s: str) -> str:
+    """
+    Reverses the order of the vowels in the given string `s`.
+
+    This function first converts the input string `s` to a list `s`. It then creates an empty list `res` and appends the indices
+    of the characters in `s` to `res`. The function then iterates over half of the length of `res`, swapping the characters
+    at the indices `res[i]` and `res[-i-1]` in `s`. Finally, the function joins the characters in `s` back into a string
+    and returns the result.
+
+    Args:
+        s (str): The input string containing vowels to reverse.
+
+    Returns:
+        str: The input string `s` with the vowels reversed.
+        
+        
+    Time Complexity = O(n)
+    Space Complexity = O(n)
+
+    """
+    res = []
+    s = list(s)
+    for i in range(len(s)):
+        res.append(i)
+    for i in range(len(res)//2):
+        s[res[i]], s[res[-i-1]] = s[res[-i-1]], s[res[i]]
+    return ''.join(s)
