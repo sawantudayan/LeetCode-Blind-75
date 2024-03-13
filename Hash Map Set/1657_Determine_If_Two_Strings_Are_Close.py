836f0c0e3345e00f07a0b04131f7992485391b1d
Udayan Sawant <udayansawant7@gmail.com>
2024-03-12 20:54:34 -0700

    DAY 8 - Hash Map/Set

---
+++ Hash Map Set/1657_Determine_If_Two_Strings_Are_Close.py
@@ -0,0 +1,13 @@
+from collections import Counter
+
+def closeStrings(word1: str, word2: str) -> bool:
+   """
+   This function checks if two given strings are close strings or not.\n
+   Two strings are said to be close if they contain the same letters in their integer counts, up to rearrangement, and have the same length.\n
+   
+   Time Complexity: O(n log n) due to the sorting operation\n
+   Space Complexity: O(n) as we are storing the counts of characters in the string.
+   """
+   cnt1 = Counter(word1)
+   cnt2 = Counter(word2)
+   return sorted(cnt1.values()) == sorted(cnt2.values()) and set(cnt1.keys()) == set(cnt2.keys())
